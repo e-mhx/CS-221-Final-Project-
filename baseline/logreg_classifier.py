8399aa82c09b64b29cc484ec09f1c114f589114a
Austin Cai <acai21@stanford.edu>
2019-05-24 16:24:09 -0700

    extract pkl file, converting each entry into a dictionary of word count. This can be fed into a logistic classifier (which has not been implemented)

--- baseline/logreg_classifier.py
+++ baseline/logreg_classifier.py
@@ -1,6 +1,16 @@
-test_data = [
-"I am a very happy person and life is going so well", \
-"I am depressed and do not want to live anymore", \
-"Why is my boss so mean to me", \
-"I love my friends and the people in my life", \
-]
+import pickle
+import collections
+
+COMMENT_INDEX = 17
+
+with open('../data/condensed_dataset_SMALL.pkl', 'rb') as f:
+    dataset = pickle.load(f) # dataset is the list of entries 
+
+# creates a dictionry contain word counts for each entry
+# these dictionaries will eventually be fed into a logistic classifier
+for entry in dataset:
+	entryDict = collections.defaultdict(int)
+
+	entryList = entry[COMMENT_INDEX].split()
+	for word in entryList:
+		entryDict[word] += 1
