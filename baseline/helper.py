db2b1af2cb60f9af6c14f99589a1aaa4bbf0f4f1
Austin Cai <acai21@stanford.edu>
2019-06-02 11:21:17 -0700

    finished one-all logistic regression, it basically has 0% accuracy

--- baseline/helper.py
+++ baseline/helper.py
@@ -3,17 +3,57 @@ import collections
 import string
 import re
 
-COMMENT_INDEX = 17
-LAPALCE = 1
 
-testDataset = [
+tinyDataset = [
 	["friend!", 0],
 	["friend good!", 0], 
+	["I love you so much", 0],
+	["do you want to see a movie?", 0],
+	["good friend", 0],
 	["enemy!", 1],
-	["enemy bad!", 1]
+	["enemy bad!", 1],
+	["I hate you", 1],
+	["get out of my sight", 1],
+	["hate enemy", 1],
 ]
 
-def learnProbs(dataset):
+smallDataset = [
+	["friend!", 0],
+	["friend good!", 0], 
+	["I love you so much", 0],
+	["do you want to see a movie?", 0],
+	["you are so important to me", 0],
+	["there is no one I'd rather spend time with than you", 0],
+	["we should see each other more", 0],
+	["I'm so happy we got closer", 0],
+	["happy important good", 0],
+	["I want to spend time with you so much", 0],
+	["enemy!", 1],
+	["enemy bad!", 1],
+	["I hate you", 1],
+	["get out of my sight", 1],
+	["evil exists because of people like you", 1],
+	["I don't want to see you", 1],
+	["I've never met anyone as incompetant as you", 1],
+	["can you please leave me be", 1],
+	["I hate you more than anyone", 1],
+	["I will never like you", 1],
+]
+
+def partitionDataset(dataset, catCount, testProp): 
+	trainSet = []
+	testSet = []
+
+	for i in range(len(dataset)):
+		if i % (len(dataset)/catCount) >= (len(dataset)/catCount) * testProp:
+			testSet.append(dataset[i])
+		else:
+			trainSet.append(dataset[i])
+
+	return trainSet, testSet
+
+
+def learnProbs(dataset, laplace, commentIdx):
 
 	ingroupEntryCount = 0
 
@@ -22,7 +62,7 @@ def learnProbs(dataset):
 	outgroupWordCount = collections.defaultdict(float)
 	for entry in dataset:
 
-		entryList = entry[COMMENT_INDEX].split()
+		entryList = entry[commentIdx].split()
 		if entry[-1] == 0: # if the current entry is classified in subreddit 1
 			ingroupEntryCount += 1
 			for word in entryList:
@@ -30,7 +70,7 @@ def learnProbs(dataset):
 				ingroupWordCount[normalizedWord] += 1
 
 		else: # if the current entry is NOT classified in subreddit 1
-			entryList = entry[COMMENT_INDEX].split()
+			entryList = entry[commentIdx].split()
 			for word in entryList:
 				normalizedWord = re.sub(r'[^\w\s]','',word.lower())
 				outgroupWordCount[normalizedWord] += 1
@@ -39,9 +79,30 @@ def learnProbs(dataset):
 	ingroupWordProb = collections.defaultdict(float)
 	outgroupWordProb = collections.defaultdict(float)
 	for key, val in ingroupWordCount.iteritems():
-		ingroupWordProb[key] = (val+LAPALCE)/(ingroupEntryCount + 2*LAPALCE)
+		ingroupWordProb[key] = (val+laplace)/(ingroupEntryCount + 2*laplace)
 
 	for key, val in outgroupWordCount.iteritems():
-		outgroupWordProb[key] = (val+LAPALCE)/(len(dataset) - ingroupEntryCount + 2*LAPALCE)
+		outgroupWordProb[key] = (val+laplace)/(len(dataset) - ingroupEntryCount + 2*laplace)
 
-	return ingroupWordProb, outgroupWordProb
+	return ingroupWordProb, outgroupWordProb, ingroupEntryCount
+
+def classAndEval(testSet, ingroupWordProb, outgroupWordProb, ingroupEntryCount, outgroupEntryCount, laplace, commentIdx):
+	
+	correctInCount = 0
+	correctOutCount = 0
+	for entry in testSet:
+		inWeight = 1.0
+		outWeight = 1.0
+
+		entryList = entry[commentIdx].split()
+		for word in entryList:
+
+			inWeight *= ingroupWordProb[word] if ingroupWordProb[word] != 0 else float(laplace)/(ingroupEntryCount + 2*laplace)
+			outWeight *= outgroupWordProb[word] if outgroupWordProb[word] != 0 else float(laplace)/(outgroupEntryCount + 2*laplace)
+
+		if inWeight > outWeight and entry[-1] == 0:
+			correctInCount+=1
+		if outWeight > inWeight and entry[-1] > 0:
+			correctOutCount+=1
+
+	return correctInCount, correctOutCount
--- baseline/logreg_classifier.py
+++ baseline/logreg_classifier.py
@@ -4,9 +4,31 @@ import string
 import helper
 import re 
 
-with open('../data/condensed_dataset_SMALL.pkl', 'rb') as f:
+SMALL_DATASET = '../data/condensed_dataset_SMALL.pkl'
+FULL_DATASET = '../../data/condensed_dataset.pkl'
+
+CATEGORY_COUNT = 2
+TESTSET_PROPORTION = 0.80
+
+LAPLACE = 1
+COMMENT_INDEX = 17
+INENTRY_COUNT = 40000 # number of ingroup entries in the data set
+OUTENTRY_COUNT = 760000
+
+with open(FULL_DATASET, 'rb') as f:
     dataset = pickle.load(f) # dataset is the list of entries 
 
-ingroupWordProb, outgroupWordProb = helper.learnProbs(dataset)
+#splits dataset into test set and train set 
 
-# TODO: when infering, use laplace !!
+
+trainSet, testSet = helper.partitionDataset(dataset, CATEGORY_COUNT, TESTSET_PROPORTION)
+
+ingroupWordProb, outgroupWordProb, ingroupEntryCount = helper.learnProbs(trainSet, LAPLACE, COMMENT_INDEX)
+
+correctInCount, correctOutCount = helper.classAndEval(testSet, ingroupWordProb, outgroupWordProb, INENTRY_COUNT, OUTENTRY_COUNT, LAPLACE, COMMENT_INDEX)
+
+# when run on FULL_DATASET
+# 0/10,000
+# 35/190,000
+print correctInCount
+print correctOutCount
